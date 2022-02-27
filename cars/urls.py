58fb8248694c0e8e67e7339a00971e135af320f2
mayurgorane2311 <mayurgorane23@gmail.com>
2022-02-27 23:11:30 +0530

    search form and search fuctionality

--- cars/urls.py
+++ cars/urls.py
@@ -1,9 +1,9 @@
-
-from unicodedata import name
 from . import views
 from django.urls import path
 
 
 urlpatterns = [
    path('',views.cars,name= 'cars'),
+   path('<int:id>',views.car_detail,name='car_detail'),
+   path('search',views.search,name= 'search'), 
 ]
